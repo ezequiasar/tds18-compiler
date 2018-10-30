27553d0873b47ad1366bd638d1cce19b7a45a0ac
Santiago Maria <santimaria93@gmail.com>
2018-10-30 17:47:32 -0300

    added instruction to open and end function; impĺemented method call instruction

--- intermediate_code.c
+++ intermediate_code.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <structs.h>
 
+#define BEGIN_FUN 'b' + 'f'
+#define END_FUN 'e' + 'f'
+/////////////////////////////////////////
 #define IF 'i'
 #define IF_BODY 'i' + 'b'
 #define IF_COND 'i' + 'c'
@@ -50,28 +53,54 @@ InstructionNode * generate_intermediate_code(ASTNode * root) {
 	if (root != NULL) {
     add_instruction(create_instruction_from_ASTNode(root));
     InstructionNode * root_ins = last;
-    InstructionNode * left_ins = generate_intermediate_code(root -> left_child);
-    InstructionNode * right_ins = generate_intermediate_code(root -> right_child);
-
-    switch(root_ins -> operation) {
-      case IF:
-        left_ins -> operation = IF_COND;
-        break;
-      case IF_BODY:
-        left_ins -> operation = IF_THEN;
-        if (right_ins != NULL)
-          right_ins -> operation = IF_ELSE;
-        break;
-      case WHILE:
-        left_ins -> operation = WHILE_COND;
-        right_ins -> operation = WHILE_BODY;
-        break;
+    /****
+      *
+      * Si es un nodo de llamada a una funcion, entonces
+      *   op1 debe ser un nodo que guarda el nombre de la funcion a llamar
+      *   op2 debe ser una lista de los parametros actuales de la llamada
+      *   LOS PARAMETROS ESTAN GUARDADOS COMO UNA LISTA AST EN EL ARBOL.- Ver ast_from_parameters_list en calc-sintaxis.y
+      *
+    */
+    if (root_ins -> operation != METH_CALL) {
+      InstructionNode * left_ins = generate_intermediate_code(root -> left_child);
+      InstructionNode * right_ins = generate_intermediate_code(root -> right_child);
+      switch(root_ins -> operation) {
+        case IF:
+          left_ins -> operation = IF_COND;
+          break;
+        case IF_BODY:
+          left_ins -> operation = IF_THEN;
+          if (right_ins != NULL)
+            right_ins -> operation = IF_ELSE;
+          break;
+        case WHILE:
+          left_ins -> operation = WHILE_COND;
+          right_ins -> operation = WHILE_BODY;
+          break;
+      }
+      if (left_ins != NULL)
+        root_ins -> op1 = left_ins -> result;
+      if (right_ins != NULL)
+        root_ins -> op2 = right_ins -> result;
     }
-
-    if (left_ins != NULL)
-      root_ins -> op1 = left_ins -> result;
-    if (right_ins != NULL)
-      root_ins -> op2 = right_ins -> result;
+    else {
+      ASTNode * ast_aux = root;
+      VarNode * varnode_aux;
+      VarNode * op1 = create_temporal(root -> function_data -> id);
+      root_ins -> op1 = op1;
+      VarNode * op2 = NULL;
+      while (ast_aux != NULL) {
+        if (op2 == NULL) {
+          op2 = ast_aux -> var_data;
+          varnode_aux = op2;
+        }
+        else {
+          varnode_aux -> next = ast_aux -> var_data;
+          varnode_aux = varnode_aux -> next;
+        }
+        ast_aux = ast_aux -> right_child;
+      }
+    }
     return last;
   }
   return NULL;
@@ -81,7 +110,7 @@ InstructionNode * generate_intermediate_code(ASTNode * root) {
 	Recorre el cuerpo de una funcion y crea las instrucciones en codigo intermedio
 */
 InstructionNode * create_instruction_from_ASTNode(ASTNode * root) {
-  InstructionNode * new_node = malloc(sizeof(InstructionNode));
+  InstructionNode * new_node = malloc((InstructionNode *) sizeof(InstructionNode));
   // in every case we need to set the new_node -> operation field.
   switch (root -> node_type) {
     case _if:
@@ -138,12 +167,28 @@ InstructionNode * create_instruction_from_ASTNode(ASTNode * root) {
   return new_node;
 }
 
-void generate_fun_name_code(char * id) {
+InstructionNode * generate_fun_name_code(char * id) {
+  InstructionNode * init_fun_ins = malloc((InstructionNode *) sizeof(InstructionNode));
+  VarNode * fun_temp_varnode = create_temporal(id);
+  init_fun_ins -> operation = BEGIN_FUN;
+  init_fun_ins -> op1 = fun_temp_varnode;
+  init_fun_ins -> op2 = NULL;
+  init_fun_ins -> result = NULL;
+  init_fun_ins -> back = NULL;
+  init_fun_ins -> next = NULL;
+  return init_fun_ins;
 
 }
 
-void generate_fun_name_end(char * id) {
-
+InstructionNode * generate_fun_name_end(VarNode * fun_temp_varnode) {
+  InstructionNode * end_fun_ins = malloc((InstructionNode *) sizeof(InstructionNode));
+  init_fun_ins -> operation = END_FUN;
+  init_fun_ins -> op1 = fun_temp_varnode;
+  init_fun_ins -> op2 = NULL;
+  init_fun_ins -> result = NULL;
+  init_fun_ins -> back = NULL;
+  init_fun_ins -> next = NULL;
+  return end_fun_ins;
 }
 /*
 	Funcion que recorre la lista de funciones y para cada una:
@@ -152,9 +197,12 @@ void generate_fun_name_end(char * id) {
 		3) Cierra el bloque de funcion END_FUN_<ID>
 **/
 CodeNode * generate_fun_code(FunctionNode * root) {
-	generate_fun_name_code(root -> id);
+	InstructionNode * init_fun_ins = generate_fun_name_code(root -> id);
+  InstructionNode * end_fun_ins = generate_fun_name_end(init_fun_ins -> op1);
+  add_instruction(init_fun_ins);
 	generate_intermediate_code(root -> body);
-	generate_fun_name_end(root -> id);
+  add_instruction(end_fun_ins);
+	
 	return NULL;
 }
 
