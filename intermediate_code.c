975533cf8ec2eb1509bbcbc5bd1734df5b0025da
Santiago Maria <santimaria93@gmail.com>
2018-10-30 16:54:11 -0300

    added operation to all created instructions

--- intermediate_code.c
+++ intermediate_code.c
@@ -2,14 +2,16 @@
 #include <structs.h>
 
 #define IF 'i'
+#define IF_BODY 'i' + 'b'
 #define IF_COND 'i' + 'c'
 #define IF_THEN 'i' + 't'
 #define IF_ELSE 'i' + 'e'
 #define WHILE 'w'
 #define WHILE_COND 'w' + 'c'
+#define WHILE_BODY 'w' + 'b'
 //////////////////////////////////////////
 #define PLUS '+'
-#define MUNUS '-'
+#define MINUS '-'
 #define PROD '*'
 #define DIV '/'
 #define MOD '%'
@@ -22,6 +24,10 @@
 /////////////////////////////////////////
 #define AND '&'
 #define OR '|'
+#define NOT '!'
+/////////////////////////////////////////
+#define RETURN 'r'
+#define METH_CALL 'm' + 'c'
 
 InstructionNode * head, *last;
 
@@ -46,6 +52,22 @@ InstructionNode * generate_intermediate_code(ASTNode * root) {
     InstructionNode * root_ins = last;
     InstructionNode * left_ins = generate_intermediate_code(root -> left_child);
     InstructionNode * right_ins = generate_intermediate_code(root -> right_child);
+
+    switch(root_ins -> operation) {
+      case IF:
+        left_ins -> operation = IF_COND;
+        break;
+      case IF_BODY:
+        left_ins -> operation = IF_THEN;
+        if (right_ins != NULL)
+          right_ins -> operation = IF_ELSE;
+        break;
+      case WHILE:
+        left_ins -> operation = WHILE_COND;
+        right_ins -> operation = WHILE_BODY;
+        break;
+    }
+
     if (left_ins != NULL)
       root_ins -> op1 = left_ins -> result;
     if (right_ins != NULL)
@@ -63,15 +85,52 @@ InstructionNode * create_instruction_from_ASTNode(ASTNode * root) {
   // in every case we need to set the new_node -> operation field.
   switch (root -> node_type) {
     case _if:
+      new_node -> operation = IF;
+      break;
     case _if_body:
+      new_node -> operation = IF_BODY;
+      break;
     case _while:
+      new_node -> operation = WHILE;
+      break;
     case _arith_op:
+      if (root -> data == '+')
+        new_node -> operation = PLUS;
+      else if (root -> data == '-')
+        new_node -> operation = MINUS;
+      else if (root -> data == '*')
+        new_node -> operation = PROD;
+      else if (root -> data == '/')
+        new_node -> operation = DIV;
+      else //root -> data == '%'
+        new_node -> operation = MOD;
+      break;
     case _boolean_op:
+      if (root -> data == '>')
+        new_node -> operation = GRATER_THAN;
+      else if (root -> data == '<')
+        new_node -> operation = LESSER_THAN;
+      else if (root -> data == 'e')
+        new_node -> operation = EQUALS;
+      else if (root -> data == '&')
+        new_node -> operation = AND;
+      else if (root -> data == '|')
+        new_node -> operation = OR;
+      else //root -> data == '!'
+        new_node -> operation = NOT;
+      break;
     case _assign:
+      new_node -> operation = ASSIGN;
+      break;
     case _method_call:
+      new_node -> operation = METH_CALL;
+      break;
     case _return:
-    case _id:
-    case _literal:
+      new_node -> operation = RETURN;
+      break;
+    default:
+      new_node -> operation = -1;
+      break;
   }
   new_node -> result = create_temporal("aca va el nombre del temporal");
   new_node -> back = NULL;
